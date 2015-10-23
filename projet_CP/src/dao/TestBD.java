933eaa84bbbc0a731492f75046d6f395baa942be
oconstan <m.constans.olivier@gmail.com>
2015-10-23 18:15:43 +0200

    Implementation du model utilisateur terminee

--- projet_CP/src/dao/TestBD.java
+++ projet_CP/src/dao/TestBD.java
@@ -1,16 +1,32 @@
 package dao;
 
+import java.util.List;
+
 import beans.Projet;
 import beans.Utilisateur;
 
 public class TestBD {
 
 	public static void main(String[] args) {
-		testProjet();
+		testUtilisateur();
+		//testProjet();
 		/*IUtilisateurDAO utilisateur = new UtilisateurDAOimpl();
 		Utilisateur u = new Utilisateur("paulseb", "mdp", "paul", "sebastien");
 		utilisateur.ajouter(u);
 		System.out.println(u.getIdUtilisateur());*/
+	}
+	
+	public static void testUtilisateur(){
+		IUtilisateurDAO utilisateurDAO = new UtilisateurDAOimpl();
+		Utilisateur u = utilisateurDAO.recupererUtilisateur("Olivier");
+		System.out.println(utilisateurDAO.existanceIdentifiant("Olivier"));
+		System.out.println(utilisateurDAO.existanceIdentifiant("OlivierR"));
+		System.out.println(utilisateurDAO.verificationConnexion("Olivier", "password"));
+		System.out.println(utilisateurDAO.verificationConnexion("Olivier", "passrword"));
+		u.setNom("ConStanS");
+		utilisateurDAO.modifier(u);
+		List<Utilisateur> list = utilisateurDAO.lister();
+		System.out.println(list);
 	}
 	
 	public static void testProjet(){
