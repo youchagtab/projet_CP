15ce2633799c07874555eb5c5274db86efe2e5e3
euskanani <euskanani@crab>
2015-11-16 20:54:30 +0100

    modifs sur les collaborateurs

--- projet_CP/src/controller/supprimerCollaborateur.java
+++ projet_CP/src/controller/supprimerCollaborateur.java
@@ -25,56 +25,8 @@ public class supprimerCollaborateur extends HttpServlet{
 
 	
 	private static final long serialVersionUID = 1L;
-	public static final String VUE_AJOUTER_Collab = "/restreint/ajouterCollaborateur.jsp";
-	public static final String VUE_PROJET = "/projet_CP/projet";
 	public static final String PARAM_ID_PROJET = "idProjet";
-	public static final String ATT_PROJET = "projet";
-	public static final String ATT_COLLABORATEURS = "collaborateurs";
-	public static final String ATT_UTILISATEURS = "utilisateurs";
-	public static final String IDs_UTILISATEURS_A_AJOUTER= "ajoutUtilisateurs";
 	public static final String IDs_COLLABORATEURS_A_SUPPRIMER= "ajoutCollaborateurs";
-
-
-	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-		String idProjet = request.getParameter(PARAM_ID_PROJET);
-
-		IProjetDAO projetDAO =new ProjetDAOimpl();
-		beans.Projet projet = projetDAO.recupererProjet(Integer.parseInt(idProjet));
-		IProjetUtilisateurDAO projetUtilisateur = new ProjetUtilisateurDAOimpl();
-		List<Integer> collaborateursIDs = projetUtilisateur.listerIdUtilisateurs(Integer.parseInt(idProjet));
-
-
-
-		/*recuperation des  collaborateurs*/
-		List<Utilisateur> collaborateurs = new ArrayList<Utilisateur>();
-		IUtilisateurDAO utilisateurDAO =new UtilisateurDAOimpl();
-
-
-		Iterator<Integer> iter = collaborateursIDs.iterator();
-		while (iter.hasNext()) {
-			beans.Utilisateur utilisateur = utilisateurDAO.recupererUtilisateur(iter.next());
-			collaborateurs.add(utilisateur);
-		}
-
-		Iterator<Utilisateur> collab = collaborateurs.iterator();
-		/*recuperation des utilisateurs non collaborateurs sur le projet*/
-		List<Utilisateur> utilisateurs =utilisateurDAO.lister();
-
-		while (collab.hasNext()) {
-			utilisateurs.remove(collab.next());	
-		}
-
-
-		request.setAttribute(ATT_PROJET, projet);
-		request.setAttribute(ATT_UTILISATEURS, utilisateurs);
-		request.setAttribute(ATT_COLLABORATEURS, collaborateurs);
-
-		this.getServletContext().getRequestDispatcher(VUE_AJOUTER_Collab).forward(request, response);
-
-
-	}
 
 
 	@Override
@@ -99,9 +51,7 @@ public class supprimerCollaborateur extends HttpServlet{
 			System.out.println("pas de choix faits pour retirer");
 		}
 
-
-
-		doGet(request, response);
+        response.sendRedirect("AjouterCollaborateurs?idProjet="+idProjet);
 
 
 	}
