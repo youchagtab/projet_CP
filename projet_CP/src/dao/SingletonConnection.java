526c0d0c7d74ccc61cc67f93cc08ba50a13495a1
telgamrani <telgamrani@glengarioch.emi.u-bordeaux1.fr>
2015-11-25 20:25:49 +0100

    commit singleton

--- projet_CP/src/dao/SingletonConnection.java
+++ projet_CP/src/dao/SingletonConnection.java
@@ -9,7 +9,9 @@ public class SingletonConnection {
 	static {
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-			connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/oconstan", "root", "");
+			connection = DriverManager.getConnection(                  //jdbc:mysql://dbserver:3306/oconstan
+					"jdbc:mysql://dbserver.emi.u-bordeaux.fr/oconstan", "oconstan", 
+					"U-bordeaux1");
 
 		} catch (Exception e) {
 			e.printStackTrace();
